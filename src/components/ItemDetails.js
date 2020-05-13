b45bd7337b6966cdf70cd5fcc71a87f906dc5b43
Tarun Kumar <tarun12.tarunkr@gmail.com>
2020-05-13 23:59:26 +0530

    state added for ItemDetails All the state for ItemDetails added.

--- src/components/ItemDetails.js
+++ src/components/ItemDetails.js
@@ -3,6 +3,17 @@ import {Container, Row, Col} from 'react-bootstrap'
 import Label from "./Label";
 
 class ItemDetails extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            category : this.props.details.Category,
+            title: this.props.details.title,
+            dueDate: this.props.details.dueDate,
+            createDate: this.props.details.createDate,
+            priority: this.props.details.priority,
+            status: this.props.details.status
+        }
+    }
     render() {
         const Style = {
             ColPad: {
@@ -15,21 +26,21 @@ class ItemDetails extends React.Component {
                         <Row>
                             <Col xs={3}>
                                 <Container className="title_category">
-                                    {this.props.details.Category}
+                                    {this.state.category}
                                 </Container>
                             </Col>
                             <Col xs={9}>
-                                <Label label="Title:" value={this.props.details.title}/>
-                                <Label label="Due Date:" value={this.props.details.dueDate}/>
-                                <Label label="Create Date:" value={this.props.details.createDate}/>
+                                <Label label="Title:" value={this.state.title}/>
+                                <Label label="Due Date:" value={this.state.dueDate}/>
+                                <Label label="Create Date:" value={this.state.createDate}/>
                             </Col>
                         </Row>
                     </Col>
                     <Col md={4} style={Style.ColPad}>
                         <Row>
                             <Col xs={12}>
-                                <Label label="Priority:" value={this.props.details.priority}/>
-                                <Label label="Status: " value={this.props.details.status}/>
+                                <Label label="Priority:" value={this.state.priority}/>
+                                <Label label="Status: " value={this.state.status}/>
                                 <Label label="Buttons"/>
                             </Col>
                         </Row>
